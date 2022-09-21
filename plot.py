5cb5cd76c1eb9a922421e965a85b4d8a0b31bcd3
Jeff Shen <shenjeff@princeton.edu>
2022-09-20 23:24:19 -0400

    plotting script gets even uglier

--- plot.py
+++ plot.py
@@ -14,20 +14,29 @@ parser.add_argument(
 )
 parser.add_argument("--var", help="Variable to plot", default="jx", type=str)
 parser.add_argument(
-    "--passes", help="Number of passes with filter", default=0, type=int
+    "--passes", nargs="+", help="Number of passes with filter", default=0, type=int
 )
 parser.add_argument(
     "--to_filter", nargs="+", help="Which of the plots to filter", type=int
 )
+parser.add_argument("--title", help="Figure title", type=str)
 parser.add_argument(
-    "--filter", help="Filter to apply", choices=["gaussian", "median"], type=str
+    "--filter",
+    help="Filter to apply",
+    choices=["gaussian", "median", "uniform"],
+    type=str,
 )
-parser.add_argument("--flip", nargs="+", help="Which of the plots to flip", type=int)
+parser.add_argument("--flip", nargs="+", help="Which of the plots to invert", type=int)
+parser.add_argument(
+    "--no_flip",
+    action="store_true",
+    help="Don't invert any of the plots",
+)
 args = parser.parse_args()
 
 
 n = len(args.files)
-fig, ax = plt.subplots(1, n, figsize=(6 + 5 * n, 5))
+fig, ax = plt.subplots(1, n, figsize=(6 + 5 * n, 4))
 if n == 1:
     ax = [ax]
 
@@ -41,24 +50,41 @@ if args.flip is not None:
 else:
     to_flip = list(range(1, n))
 
+if args.no_flip:
+    to_flip = []
+
+if len(args.passes) == 1:
+    passes = args.passes * n
+else:
+    assert (
+        len(args.passes) == n
+    ), "Number of passes must be 1 or equal to number of files"
+    passes = args.passes
+
+
 for i, fname in enumerate(args.files):
     f = h5py.File(fname, "r")
     current = f[args.var][:]
     if current.ndim == 3:
         current = current[0, :, :]
     if i in to_filter:
-        for _ in range(args.passes):
-            if args.filter == "gaussian":
-                filter = np.matrix("1 2 1; 2 4 2; 1 2 1") / 16.0
-                current = convolve2d(current, filter, mode="same")
-            elif args.filter == "median":
+        for _ in range(passes[i]):
+            if args.filter == "median":
                 current = medfilt2d(current, kernel_size=3)
+            else:
+                if args.filter == "gaussian":
+                    filter = np.matrix("1 2 1; 2 4 2; 1 2 1") / 16.0
+                elif args.filter == "uniform":
+                    filter = np.ones((3, 3)) / 9.0
+                current = convolve2d(current, filter, mode="same")
     if i in to_flip:
         current *= -1
     im = ax[i].imshow(current)
     ax[i].set_title(
-        f"{args.labels[i]} {'+ ' + str(args.passes) + ' passes' if i in to_filter else ''}"
+        f"{args.labels[i]} {'+ ' + str(passes[i]) + ' ' + args.filter + ' passes' if i in to_filter and passes[i] > 0 else ''}"
     )
     plt.colorbar(im, ax=ax[i])
     f.close()
+
+plt.suptitle(args.title)
 plt.show()
