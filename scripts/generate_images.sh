d7c9b0a1bcc2a225d1e14722c552865107de3a60
Jeff Shen <shenjeff@princeton.edu>
2022-09-21 15:44:31 -0400

    update image generation script

--- scripts/generate_images.sh
+++ scripts/generate_images.sh
@@ -1,14 +1,19 @@
+#!/bin/bash
 # $1 is the tristan output directory
 # $2 is the path to the deposit_particles binary
 
-echo $1 $2
-
-n=$(ls -l $1 |rg prtl |sort |tail -n1 |sed 's/prtl.tot.//g')
+n=$(ls $1 |rg prtl |sort |tail -n1 |sed 's/prtl.tot.//g')
 ppcs=(1600 32 16 4)
 for ppc in "${ppcs[@]}"; do
-  for i in $(seq 1 $n); do
+    for i in $(seq -w 1 $n); do
     thin=$((1600 / ppc))
-    $2 --field-file $1/flds.tot.$i --particle-file $1/prtl.tot.$i --param-file $1/param.$1 --thin $thin --output-file out-$ppc.$i
-    echo done $i of $n for ppc=$ppc
+    fname=out-$ppc.$i
+    if [ -f "$fname" ]; then
+	    echo skipping $i of $n for ppc=$ppc
+    else
+	    $2 --field-file $1/flds.tot.$i --particle-file $1/prtl.tot.$i --param-file $1/param.$i --thin $thin --output-file $fname &
+
+	    echo done $i of $n for ppc=$ppc
+    fi
   done
 done
